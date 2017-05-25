b3c85ed11897a4f2e310ffe296688f4fbfef5807
Maëlle Salmon <maelle.salmon@yahoo.se>
2017-05-25 23:08:58 +0200

    starts using @karawoo's EML as example :angel:

--- inst/notebook/testing.R
+++ inst/notebook/testing.R
@@ -1,5 +1,5 @@
 
-eml <- xml2::read_xml("inst/extdata/Rodents_snakes_and_raptors.xml")
+eml <- xml2::read_xml("inst/extdata/Kara_EML.xml")
 style <- xml2::read_xml("inst/xsl/eml/eml.xsl", package = "emldown")
 html <- xslt::xml_xslt(eml, style)
 xml2::write_html(html, "test.html")
