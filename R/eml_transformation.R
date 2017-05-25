1239f06adae005f46a206294f33a86aaea3e7eab
Maëlle Salmon <maelle.salmon@yahoo.se>
2017-05-25 19:50:46 +0200

    corrects code

--- R/eml_transformation.R
+++ R/eml_transformation.R
@@ -1,7 +1,7 @@
 library("xslt")
 
 eml <- read_xml(system.file("xsd/test/eml-datasetWithUnits.xml", package = "EML"))
-style <- read_xml("style/eml/eml.xsl")
+style <- read_xml("inst/style/eml/eml.xsl")
 html <- xml_xslt(eml, style)
 write_html(html, "test.html")
 
@@ -10,6 +10,6 @@ library("EML")
 eml <- read_eml(system.file("xsd/test/eml-datasetWithUnits.xml", package = "EML"))
 dt <- EML::eml_get(eml, "dataTable")
 eml <- read_xml("dataTable.xml")
-style <- read_xml("style/eml/eml-datatable.xsl")
+style <- read_xml("inst/style/eml/eml-datatable.xsl")
 html <- xml_xslt(eml, style)
 write_html(html, "test_datatable.html")
