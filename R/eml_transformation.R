24e2d1445ed7169b242920c3dd8ea7b402b8debe
Maëlle Salmon <maelle.salmon@yahoo.se>
2017-05-25 21:26:25 +0200

    changes the EML example to one with a newer EML version

--- R/eml_transformation.R
+++ R/eml_transformation.R
@@ -4,13 +4,13 @@
 #' @export
 #'
 eml_transform <- function(){
-  eml <- xml2::read_xml(system.file("extdata/Lake_Baikal_Plankton.xml", package = "emldown"))
+  eml <- xml2::read_xml(system.file("extdata/Rodents_snakes_and_raptors.xml", package = "emldown"))
   style <- xml2::read_xml((system.file("xsl/eml/eml.xsl", package = "emldown")))
   html <- xslt::xml_xslt(eml, style)
   xml2::write_html(html, "inst/test.html")
   
   # datatable
-  eml <- xml2::read_xml(system.file("extdata/Lake_Baikal_Plankton.xml", package = "emldown"))
+  eml <- xml2::read_xml(system.file("extdata/Rodents_snakes_and_raptors.xml", package = "emldown"))
   dt <- EML::eml_get(eml, "dataTable")
   eml <- xml2::read_xml(system.file("dataTable.xml", package="emldown"))
   style <- xml2::read_xml(system.file("style/eml/eml-datatable.xsl", package = "emldown"))
