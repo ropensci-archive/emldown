c5ad7cd20da0b0b1b405e04720103f0bdf85fe3a
Maëlle Salmon <maelle.salmon@yahoo.se>
2017-05-25 19:59:56 +0200

    suppresses library

--- R/eml_transformation.R
+++ R/eml_transformation.R
@@ -3,22 +3,18 @@
 #' @return
 #' @export
 #'
-#' @examples
 eml_transform <- function(){
-  library("xslt")
-  
-  eml <- read_xml(system.file("xsd/test/eml-datasetWithUnits.xml", package = "EML"))
-  style <- read_xml((system.file("inst/style/eml/eml.xsl", package = "emldown")))
-  html <- xml_xslt(eml, style)
-  write_html(html, "inst/test.html")
+  eml <- xml2::read_xml(system.file("xsd/test/eml-datasetWithUnits.xml", package = "EML"))
+  style <- xml2::read_xml((system.file("inst/style/eml/eml.xsl", package = "emldown")))
+  html <- xslt::xml_xslt(eml, style)
+  xml2::write_html(html, "inst/test.html")
   
   # datatable
-  library("EML")
-  eml <- read_eml(system.file("xsd/test/eml-datasetWithUnits.xml", package = "EML"))
+  eml <- xml2::read_eml(system.file("xsd/test/eml-datasetWithUnits.xml", package = "EML"))
   dt <- EML::eml_get(eml, "dataTable")
-  eml <- read_xml("dataTable.xml")
-  style <- read_xml(system.file("inst/style/eml/eml-datatable.xsl", package = "emldown"))
-  html <- xml_xslt(eml, style)
-  write_html(html, "inst/test_datatable.html")
+  eml <- xml2::read_xml("dataTable.xml")
+  style <- xml2::read_xml(system.file("inst/style/eml/eml-datatable.xsl", package = "emldown"))
+  html <- xslt::xml_xslt(eml, style)
+  xml2::write_html(html, "inst/test_datatable.html")
 }
 
