bc0430e8fa0b016b8d33fa09f6c4f2e0451b78bb
Maëlle Salmon <maelle.salmon@yahoo.se>
2017-05-25 19:54:19 +0200

    adds function

--- R/eml_transformation.R
+++ R/eml_transformation.R
@@ -1,15 +1,24 @@
-library("xslt")
+#' Title
+#'
+#' @return
+#' @export
+#'
+#' @examples
+eml_transform <- function(){
+  library("xslt")
+  
+  eml <- read_xml(system.file("xsd/test/eml-datasetWithUnits.xml", package = "EML"))
+  style <- read_xml((system.file("inst/style/eml/eml.xsl", package = "emldown")))
+  html <- xml_xslt(eml, style)
+  write_html(html, "inst/test.html")
+  
+  # datatable
+  library("EML")
+  eml <- read_eml(system.file("xsd/test/eml-datasetWithUnits.xml", package = "EML"))
+  dt <- EML::eml_get(eml, "dataTable")
+  eml <- read_xml("dataTable.xml")
+  style <- read_xml(system.file("inst/style/eml/eml-datatable.xsl", package = "emldown"))
+  html <- xml_xslt(eml, style)
+  write_html(html, "inst/test_datatable.html")
+}
 
-eml <- read_xml(system.file("xsd/test/eml-datasetWithUnits.xml", package = "EML"))
-style <- read_xml("inst/style/eml/eml.xsl")
-html <- xml_xslt(eml, style)
-write_html(html, "test.html")
-
-# datatable
-library("EML")
-eml <- read_eml(system.file("xsd/test/eml-datasetWithUnits.xml", package = "EML"))
-dt <- EML::eml_get(eml, "dataTable")
-eml <- read_xml("dataTable.xml")
-style <- read_xml("inst/style/eml/eml-datatable.xsl")
-html <- xml_xslt(eml, style)
-write_html(html, "test_datatable.html")
