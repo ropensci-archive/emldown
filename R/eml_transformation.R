7060c2c8feb2f732f8e540922c5caaa59e27fb74
Carl Boettiger <cboettig@gmail.com>
2017-05-25 11:11:23 -0700

    installs

--- R/eml_transformation.R
+++ R/eml_transformation.R
@@ -5,15 +5,15 @@
 #'
 eml_transform <- function(){
   eml <- xml2::read_xml(system.file("xsd/test/eml-datasetWithUnits.xml", package = "EML"))
-  style <- xml2::read_xml((system.file("inst/style/eml/eml.xsl", package = "emldown")))
+  style <- xml2::read_xml((system.file("style/eml/eml.xsl", package = "emldown")))
   html <- xslt::xml_xslt(eml, style)
   xml2::write_html(html, "inst/test.html")
   
   # datatable
-  eml <- xml2::read_eml(system.file("xsd/test/eml-datasetWithUnits.xml", package = "EML"))
+  eml <- EML::read_eml(system.file("xsd/test/eml-datasetWithUnits.xml", package = "EML"))
   dt <- EML::eml_get(eml, "dataTable")
-  eml <- xml2::read_xml("dataTable.xml")
-  style <- xml2::read_xml(system.file("inst/style/eml/eml-datatable.xsl", package = "emldown"))
+  eml <- xml2::read_xml(system.file("dataTable.xml", package="emldown"))
+  style <- xml2::read_xml(system.file("style/eml/eml-datatable.xsl", package = "emldown"))
   html <- xslt::xml_xslt(eml, style)
   xml2::write_html(html, "inst/test_datatable.html")
 }
